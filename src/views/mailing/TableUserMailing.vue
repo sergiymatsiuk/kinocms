ed8d6db767905418e8c61a386301b699cf1174a8
Sergiy Matsiuk <sergiymatsiuk1997@gmail.com>
2021-09-01 23:13:11 +0300

    create SelectUsers page from Mailing

---
+++ src/views/mailing/TableUserMailing.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card-body p-0 m-5">
+    <table class="table">
+      <thead>
+        <tr>
+          <th></th>
+          <th @click="$emit('sort-by-id')">ID</th>
+          <th>Дата регистрации</th>
+          <th @click="$emit('sort-by-BD')">День рождения</th>
+          <th>Email</th>
+          <th>Телефон</th>
+          <th @click="$emit('sort-by-name')">ФИО</th>
+          <th>Псевдоним</th>
+          <th @click="$emit('sort-by-city')">Город</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in arrUsers" :key="user.id">
+          <td><input type="checkbox" name="select" v-model="user.select" @change="$emit('changeList', user.id)"></td>
+          <td>{{user.id}}</td>
+          <td>{{user.dateReg}}</td>
+          <td>{{user.dateBD}}</td>
+          <td>{{user.mail}}</td>
+          <td>{{user.phone}}</td>
+          <td>{{user.name +' '+ user.lastName}}</td>
+          <td>{{user.nickname}}</td>
+          <td>{{user.city}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    arrUsers: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  computed: {
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style>
+  .active {
+    background-color: #BCC5D0;
+  }
+</style>
---
+++ src/views/mailing/UserFromMailing.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="card card-info">
+    <div class="align-self-center mb-3 mt-3">
+      <div
+        class="position-absolute"
+        style="width: 200px">
+        <input type="text" placeholder="поиск" v-model="search">
+      </div>
+    </div>
+    <Loader v-if="loading" class="m-5"/>
+    <div v-else>
+      <TableUserMailing
+        :arrUsers="userBySearch"
+        @changeList="changeUserList"
+        @sort-by-id="selectFilter='id'"
+        @sort-by-city="selectFilter='city'"
+        @sort-by-BD="selectFilter='dateBD'"
+        @sort-by-name="selectFilter='name'"
+      />
+      <Paginate
+        v-model="userPage"
+        :page-count="Math.ceil(userByFilter.length/userInPage)"
+        :click-handler="pageChangeHandler"
+        :prev-text="''"
+        :next-text="''"
+        :container-class="'pagination ml-5'"
+        :page-class="'page-item page-link'"
+      />
+    </div>
+    <div class="align-self-center mb-3 mt-3">
+      <button type="submit" class="btn btn-info col" style="width: 300px" @click.prevent="selectUserList">Отправить выбранным пользователям</button>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import Loader from '@/components/Loader'
+import Module from '@/module/module'
+import TableUserMailing from '@/views/mailing/TableUserMailing'
+
+export default {
+  components: {
+    Loader, TableUserMailing
+  },
+  data () {
+    return {
+      name: 'Users',
+      id: 0,
+      loading: true,
+
+      users: [],
+      search: '',
+      selectFilter: 'id',
+      userInPage: 5,
+      userPage: 1,
+    }
+  },
+  methods: {
+    async deleteUser (id) {
+      this.loading = true
+      await Module.deleteItem(this.name, id)
+      this.users = await Module.fetchInfo(this.name)
+      this.loading = false
+    },
+    pageChangeHandler (page) {
+      this.userPage = page || 1
+    },
+    changeUserList (idSelectUser) {
+      console.log(idSelectUser)
+      this.users.forEach(el => {
+        if (idSelectUser === el.id) el.select = true
+      })
+    },
+    selectUserList () {
+      console.log(this.users.filter(el => {
+        return el.select === true
+      }))
+    }
+  },
+  computed: {
+    userByFilter () {
+      const changeUsers = this.users
+
+      if (this.selectFilter === 'id') changeUsers.sort((a, b) => {return a.id - b.id})
+
+      if (this.selectFilter === 'dateBD') changeUsers.sort((a, b) => { return new Date(b.dateBD) - new Date(a.dateBD) })
+      
+      if (this.selectFilter === 'city') changeUsers.sort((a, b) => { return a.city.toLowerCase() <= b.city.toLowerCase() ? -1 : 1 })
+
+      if (this.selectFilter === 'name') changeUsers.sort((a, b) => { return a.lastName.toLowerCase() <= b.lastName.toLowerCase() ? -1 : 1 })
+      return changeUsers
+        .filter( user => {
+        let fullName = user.name +''+user.lastName
+        return fullName.includes(this.search)
+        })
+    },
+    userBySearch () {
+      return this.userByFilter.slice((this.userPage*this.userInPage-this.userInPage), (this.userPage*this.userInPage))
+    },
+  },
+  async mounted () {
+    this.id = await Module.getCounter()
+    this.users = await Module.fetchInfo(this.name)
+    this.users.forEach(el => {
+      el.select = false
+    })
+    this.loading = false
+  }
+}
+</script>
+
+<style scoped>
+  .position-absolute {
+    top: 25px;
+    right: 5%;
+  }
+</style>
